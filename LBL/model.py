6e742b24978b4c000e400012ceeb360eefbe1219
Demi Guo <demiguo98@gmail.com>
2017-10-08 16:17:43 -0400

    add model

--- LBL/model.py
+++ LBL/model.py
@@ -1,24 +1,24 @@
+import torch
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+import re
 
 class LBL(nn.Module):
-
-    # R, C, b
-    def __init__(self, vocab_size, hid_layer_size, context_size, R):
+    def __init__(self, pretrained_embeds, args):
         super(LBL, self).__init__()
-        # init configuration
-        self.vocab_size = vocab_size
-        self.context_size = context_size
-        print("vocab_size=", vocab_size)
-        print("R.shape=", R.size())
-        self.hid_layer_size = hid_layer_size
-        # embedding layers
-        self.word_embeds = nn.Embedding(vocab_size, hid_layer_size)
-        # Weight matrix, d to hidden layer
-        self.C = nn.Linear(hid_layer_size * context_size, hid_layer_size, bias=False)
-        # Bias in softmax layer
-        self.bias = nn.Parameter(torch.ones(vocab_size)).view(self.vocab_size, 1)
 
-        self.init_weight(R)
-        self.R = autograd.Variable(R)
+        self.context_size = args.context_size
+        self.hidden_size = embeds.size(1)
+        self.vocab_size = embeds.size(0)
+
+        self.embedding_layer = nn.Embedding(self.vocab_size, self.hidden_size)
+        self.context_layer = nn.Linear(self.hidden_size * self.context_size, self.hidden_size, bias=False) # C in the paper
+        self.output_layer = nn.Linear(self.hidden_size, self.vocab_size) # dot product + bias in the paper
+
+        self.init_weight(pretrained_embeds) # pretrained embeds is R in the paper
 
     def get_train_parameters(self):
         params = []
@@ -27,17 +27,21 @@ class LBL(nn.Module):
                 params.append(param)
         return params
 
-    def init_weight(self, glove_weight):
-        assert(glove_weight.size() == (self.vocab_size, self.hid_layer_size))
-        self.word_embeds.weight.data.copy_(glove_weight)
-        self.word_embeds.weight.requires_grad = False
+    def init_weight(self, pretrained_embeds):
+        assert(pretrained_weights.size() == (self.vocab_size, self.hidden_size))
+        self.embedding_layer.weight.data.copy_(pretrained_embeds)
+        self.output_layer.weight = torch.transpose(self.embedding_layer.weight, (0, 1))
 
-    def forward(self, context_vect):
-        context_vect = self.word_embeds(context_vect)
-        context_vect = context_vect.view(1, self.context_size * self.hid_layer_size)
-        model_vect = self.C(context_vect).view(self.hid_layer_size, 1)
-        final_vect = torch.mm(self.R, model_vect) + self.bias
-        final_vect = F.log_softmax(final_vect.view(self.vocab_size))
-        final_vect = final_vect.view(1, self.vocab_size)
-        return final_vect
+    def forward(self, context_words):
+        self.batch_size = context_words.size(0)
+        assert context_words.size(1) == self.context_size
+
+        embeddings = self.embedding_layer(context)
+        assert embeddings.size() == (self.batch_size, self.context_size, self.hidden_size) # sanity check
+        context_vectors = self.context_layer(embeddings.view(self.batch_size, self.context_size * self.hidden_size))
+        assert context_vectors.size() == (self.batch_size, self.hidden_size)
+        raw_outputs = self.output_layer(context_vetors)
+        assert raw_outputs.size() == (self.batch_size, self.vocab_size)
+        outputs = F.log_softmax(raw_outputs)
+        assert outputs.size() == (self.batch_size, self.vocab_size)
 
